23fa1b589d58ab681401598dab754ad6c1fea29a
yonasshah <127830788+yonasshah@users.noreply.github.com>
2024-04-15 23:35:34 -0400

    Update ProductController.cs
    changed variables

--- ProductsApp/ProductsApp/Controllers/ProductController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductController.cs
@@ -1,4 +1,4 @@
-﻿using ProductsApp.Models;
+using ProductsApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +12,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Pop", Category = "Cheese", Price = 99 },
+            new Product { Id = 2, Name = "Crackle", Category = "Boats", Price = 5.75M },
+            new Product { Id = 3, Name = "Snap", Category = "Sticks", Price = 26.99M }
         };
 
         public ProductController()
